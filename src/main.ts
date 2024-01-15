546081008fd12da2d32c28bacf531455ae1df28f
Lino <13621351+0xlino@users.noreply.github.com>
2024-01-15 16:53:32 +0000

    Init for sga-actions

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,46 @@
-import * as core from '@actions/core'
-import { wait } from './wait'
+//@ts-nocheck
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { wait } from "./wait";
+
+let staticURL =
+  "https://api.github.com/repos/Sounds-Good-Agency/expedo-store/pulls/46/files?per_page=100";
+let arrayOfFilename: any = [];
+
+async function getPaginatedData(url, octokit) {
+  const nextPattern = /(?<=<)([\S]*)(?=>; rel="Next")/i;
+  let pagesRemaining = true;
+  let data = [];
+
+  while (pagesRemaining) {
+    const response = await octokit.request(`GET ${url}`, {
+      per_page: 100,
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    });
+
+    const parsedData = parseData(response.data);
+    data = [...data, ...parsedData];
+
+    const linkHeader = response.headers.link as any;
+
+    let pagesRemaining = linkHeader && linkHeader.includes(`rel=\"next\"`);
+
+    if (pagesRemaining) {
+      url = linkHeader.match(nextPattern)[0];
+    }
+  }
+
+  return data;
+}
+
+function parseData(data: any) {
+  for (let i = 0; i < data.length; i++) {
+    arrayOfFilename.push(data[i].filename);
+  }
+  return arrayOfFilename;
+}
 
 /**
  * The main function for the action.
@@ -7,20 +48,26 @@ import { wait } from './wait'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const ms = core.getInput("milliseconds");
+    const githubToken = core.getInput("github-token", { required: true });
+    const octokit = github.getOctokit(githubToken);
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    let parsedData = await getPaginatedData(staticURL, octokit);
+    core.debug(`parsedData: ${parsedData}`);
 
+    // build this url from the github context
+    // let API_PR_URL = `https://api.github.com/repos/Sounds-Good-Agency/expedo-store/pulls/46/files?per_page=${PER_PAGE}`
+    let url = `https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/pulls/${github.context.payload.pull_request?.number}/files?per_page=100`;
+    core.debug(`url: ${url}`);
     // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug(new Date().toTimeString());
+    await wait(parseInt(ms, 10));
+    core.debug(new Date().toTimeString());
 
     // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
